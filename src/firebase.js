6d17593e8152a1a657ca20a67d3a0137ce3a96ce
Danish Pandita <49803908+DANISHPANDITA@users.noreply.github.com>
2021-06-30 20:04:29 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,7 @@
 import firebase from "firebase";
 
 const app = firebase.initializeApp({
-  apiKey: "AIzaSyCUomOPfF1UPawgOED72QPfukSOx4WtCDk",
-  authDomain: "danishpanditaportfolio.firebaseapp.com",
-  projectId: "danishpanditaportfolio",
-  storageBucket: "danishpanditaportfolio.appspot.com",
-  messagingSenderId: "921258638933",
-  appId: "1:921258638933:web:9b462f799d3b4e73c85edc",
-  measurementId: "G-JKWDBJKQRL",
+  ///////security , hahaha /////
 });
 
 const db = app.firestore();
